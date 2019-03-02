03fc4ed44d1978ac5ab6a13d8c90063dcac3e7ac
Semen Chudakov <semen.chudakov5@gmail.com>
2019-03-02 18:31:53 +0100

    readme & examples

---
+++ README.md
@@ -0,0 +1,38 @@
+# OCL
+
+Lexical analyzer of OCL language based on Cup and JFlex
+
+## Getting Started
+
+The following sections will help you download and install a copy of this project on your local machine
+
+### Prerequisites
+
+Before starting install the Java 8 as well the latest version of Maven and Git
+
+### Installing
+
+First navigate to the working directory you want the project to be in.
+Then download the source code with
+
+```
+git clone https://github.com/SChudakov/ocl.git
+```
+
+After that navigate to the project root with
+
+```
+cd ocl
+```
+
+and build the project with
+
+```
+mvn clean install
+```
+
+## Authors
+
+* **Semen Chudakov** - [SChudakov](https://github.com/SChudakov)
+* **Andrii Borziak** - [AndreaB330](https://github.com/AndreaB330)
+* **Vadym Bezdushnyi** - [VadymBezdushnyi](https://github.com/VadymBezdushnyi)
--- src/main/java/com/ocl/main/Main.java
+++ src/main/java/com/ocl/main/Main.java
@@ -3,29 +3,15 @@ package com.ocl.main;
 import com.ocl.parser.OCLParser;
 import com.ocl.scanner.OCLScanner;
 import java_cup.runtime.DefaultSymbolFactory;
-import java_cup.runtime.Symbol;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class Main {
-    private static final String CALCULATOR_TEST_FILE = "src\\main\\resources\\calcuator.txt";
-    private static final String OCL_TEST_FILE = "src\\main\\resources\\ocl.txt";
+    private static final String OCL_TEST_FILE = "src\\main\\resources\\examples\\ocl_example_1.txt";
 
     public static void main(String[] args) {
-        /*try {
-            CalculatorParser p = new CalculatorParser(new CalculatorScanner(new FileInputStream(CALCULATOR_TEST_FILE)), new DefaultSymbolFactory());
-            p.parse();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
         try {
-            /*OCLScanner scanner = new OCLScanner(new FileInputStream(OCL_TEST_FILE));
-            while (scanner.next_token().sym != 0)  {
-                System.out.println();
-            }*/
             OCLParser p = new OCLParser(new OCLScanner(new FileInputStream(OCL_TEST_FILE)), new DefaultSymbolFactory());
             p.parse();
         } catch (FileNotFoundException e) {
